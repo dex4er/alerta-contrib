ae862cc4f085bdbdd9135612258ee0fe9d52fc7a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2017-02-10 15:01:31 +0100

    All new features in one commit

--- plugins/slack/alerta_slack.py
+++ plugins/slack/alerta_slack.py
@@ -1,4 +1,3 @@
-
 import os
 import json
 import requests
@@ -7,36 +6,45 @@ import logging
 from alerta.app import app
 from alerta.plugins import PluginBase
 
+
+def convert(value, newtype, default=None):
+    try:
+        return newtype(value)
+    except (TypeError, ValueError):
+        return default
+
+
 LOG = logging.getLogger('alerta.plugins.slack')
 
 SLACK_WEBHOOK_URL = os.environ.get('SLACK_WEBHOOK_URL') or app.config['SLACK_WEBHOOK_URL']
 SLACK_ATTACHMENTS = True if os.environ.get('SLACK_ATTACHMENTS', 'False') == 'True' else app.config.get('SLACK_ATTACHMENTS', False)
-SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL') or app.config.get('SLACK_CHANNEL', '')
-ALERTA_USERNAME = os.environ.get('ALERTA_USERNAME') or app.config.get('ALERTA_USERNAME', 'alerta')
+SLACK_USERNAME = os.environ.get('SLACK_USERNAME') or app.config.get('SLACK_USERNAME')
+SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL') or app.config.get('SLACK_CHANNEL')
+SLACK_ICON_EMOJI = os.environ.get('SLACK_ICON_EMOJI') or app.config.get('SLACK_ICON_EMOJI')
+SLACK_TIMEOUT = convert(os.environ.get('SLACK_TIMEOUT'), int) or app.config.get('SLACK_TIMEOUT') or 2
+SLACK_SEVERITY_IGNORED = (os.environ.get('SLACK_SEVERITY_IGNORED') or ','.join(app.config.get('SLACK_SEVERITY_IGNORED', ['indeterminate']))).split(',')
+ALERTA_UI_URL = os.environ.get('ALERTA_UI_URL') or app.config.get('ALERTA_UI_URL', 'http://localhost')
 
-ICON_EMOJI = os.environ.get('ICON_EMOJI') or app.config.get('ICON_EMOJI', ':rocket:')
-DASHBOARD_URL = os.environ.get('DASHBOARD_URL') or app.config.get('DASHBOARD_URL', '')
 
 class ServiceIntegration(PluginBase):
+    def _send_to_slack(self, alert, changed_status=None, changed_text=None):
+        if changed_status is not None and changed_status not in ['ack', 'closed', 'open']:
+            return
 
-    def pre_receive(self, alert):
-        return alert
+        if alert.severity in SLACK_SEVERITY_IGNORED:
+            return
 
-    def post_receive(self, alert):
+        if alert.previous_severity in SLACK_SEVERITY_IGNORED and alert.severity == 'normal':
+            return
 
-        if alert.repeat:
+        if changed_status is None and alert.repeat:
             return
 
         url = SLACK_WEBHOOK_URL
 
-        summary = "*[%s] %s %s - _%s on %s_* <%s/#/alert/%s|%s>" % (
-            alert.status.capitalize(), alert.environment, alert.severity.capitalize(), alert.event, alert.resource, DASHBOARD_URL,
-            alert.id, alert.get_id(short=True)
-        )
-
-        if alert.severity == 'security':
-            color = "#000000"  # black
-        if alert.severity == 'critical':
+        if changed_status in ['ack', 'closed']:
+            color = "#00CC00"  # green
+        elif alert.severity == 'critical':
             color = "#FF0000"  # red
         elif alert.severity == 'major':
             color = "#FFA500"  # orange
@@ -44,37 +52,36 @@ class ServiceIntegration(PluginBase):
             color = "#FFFF00"  # yellow
         elif alert.severity == 'warning':
             color = "#1E90FF"  # blue
-        elif alert.severity == 'informational':
-            color = "#808080"  # gray
-        elif alert.severity == 'debug':
-            color = "#808080"  # gray
-        elif alert.severity == 'trace':
-            color = "#808080"  # gray
         else:
             color = "#00CC00"  # green
 
-        text = "<%s/#/alert/%s|%s> %s - %s" % (DASHBOARD_URL, alert.get_id(), alert.get_id(short=True), alert.event, alert.text)
+        status = changed_status or alert.status
+
+        summary = "*[%s] %s %s - _%s on %s_* <%s/#/alert/%s|%s>" % (
+            status.capitalize(), alert.environment, alert.severity.capitalize(), alert.event, alert.resource, ALERTA_UI_URL,
+            alert.id, alert.get_id(short=True)
+        )
+
+        text = "<%s/#/alert/%s|%s> %s - %s" % (ALERTA_UI_URL, alert.get_id(), alert.get_id(short=True), alert.event, alert.text)
+
+        if changed_text is not None:
+            summary += " - %s" % (changed_text)
+            text += " - %s" % (changed_text)
 
         if not SLACK_ATTACHMENTS:
 
             payload = {
-                "username": ALERTA_USERNAME,
-                "channel": SLACK_CHANNEL,
                 "text": summary,
-                "icon_emoji": ICON_EMOJI
             }
 
         else:
             payload = {
-                "username": ALERTA_USERNAME,
-                "channel": SLACK_CHANNEL,
-                "icon_emoji": ICON_EMOJI,
                 "attachments": [{
                     "fallback": summary,
                     "color": color,
                     "pretext": text,
                     "fields": [
-                        {"title": "Status", "value": alert.status.capitalize(), "short": True},
+                        {"title": "Status", "value": status.capitalize(), "short": True},
                         {"title": "Environment", "value": alert.environment, "short": True},
                         {"title": "Resource", "value": alert.resource, "short": True},
                         {"title": "Services", "value": ", ".join(alert.service), "short": True}
@@ -82,14 +89,29 @@ class ServiceIntegration(PluginBase):
                 }]
             }
 
+        if SLACK_USERNAME:
+            payload['username'] = SLACK_USERNAME
+
+        if SLACK_CHANNEL:
+            payload['channel'] = SLACK_CHANNEL
+
+        if SLACK_ICON_EMOJI:
+            payload['icon_emoji'] = SLACK_ICON_EMOJI
+
         LOG.debug('Slack payload: %s', payload)
 
         try:
-            r = requests.post(url, data=json.dumps(payload), timeout=2)
+            r = requests.post(url, data=json.dumps(payload), timeout=SLACK_TIMEOUT)
         except Exception as e:
             raise RuntimeError("Slack connection error: %s", e)
 
         LOG.debug('Slack response: %s', r.status_code)
 
-    def status_change(self, alert, status, text):
-        return
+    def pre_receive(self, alert):
+        return alert
+
+    def post_receive(self, alert):
+        self._send_to_slack(alert)
+
+    def status_change(self, alert, status=None, text=None):
+        self._send_to_slack(alert, status, text)
